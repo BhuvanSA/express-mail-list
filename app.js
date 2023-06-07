cb9c48d2cd9fff2c611614e5639230ba407c4aa9
Bhuvan S A <bhuvansa@bhuvansa.local>
2023-06-07 20:13:12 +0530

    🐞 Fixes 2

--- app.js
+++ app.js
@@ -51,6 +51,6 @@ app.post("/", function (req, res) {
   }
 });
 
-app.listen(process.env.PORT || 3000, function () {
+app.listen(process.env.PORT || 443, function () {
   console.log("Server is running");
 });
